6852108f1cbfefcbbdfb44433a2cdb7642bfbf5f
Andrew Weller <andrew@weller.fun>
2022-11-13 13:24:47 -0700

    swimming in threads

--- nm_ui/src/main.rs
+++ nm_ui/src/main.rs
@@ -1,6 +1,6 @@
 use std::{sync::{Arc, Mutex}, f32::consts::PI};
-
 use player::{Player, PlayerBuffer};
+use threadpool::ThreadPool;
 
 pub mod player;
 
@@ -11,13 +11,23 @@ fn main() {
     let mut p = p.build_stream(our_mutex.clone());
 
     let num = our_mutex.lock().unwrap().buffer_size();
-
+    let pool = ThreadPool::new(32);
+    let time_mut = Arc::new(Mutex::new(0));
 
     p.play();
-    for i in 0..num {
-        let sec = i as f32 / p.sample_rate() as f32;
-        our_mutex.lock().unwrap().write((sec * 440.0 * 2.0 * PI).sin() / 2.0);
+    for _ in 0..32 {
+        let time_c = time_mut.clone();
+        let sample_rate = p.sample_rate();
+        let buf_mut = our_mutex.clone();
+        pool.execute(move || {
+            loop {
+                let mut i = time_c.lock().unwrap();
+                *i = *i + 1;
+                let sec = *i as f32 / sample_rate as f32;
+                buf_mut.lock().unwrap().write((sec * 440.0 * 2.0 * PI).sin() / 2.0);
+            }
+        })
     }
-    std::thread::sleep(std::time::Duration::from_millis(1000));
+    std::thread::sleep(std::time::Duration::from_millis(2000));
     p.pause();
 }
